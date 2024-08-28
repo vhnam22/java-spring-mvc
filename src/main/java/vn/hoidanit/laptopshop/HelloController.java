6ea057bab498a27881fd039d26cc9ff85373c65b
Nam Vu <vhnam2209@gmail.com>
2024-08-28 11:04:29 +0700

    config, anotation

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -6,7 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String Index() {
-        return "Hello world from Spring Boot!";
+    public String index() {
+        return "Hello world";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page.";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page.";
     }
 }
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,8 +2,10 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
